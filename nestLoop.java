2a0762afe2aefdfc859b99c7d23d7e27e5e1eb81
GITLIEN <60283203+LIENGITS@users.noreply.github.com>
2022-11-07 18:13:24 -0500

    Update nestLoop.java

--- nestLoop.java
+++ nestLoop.java
@@ -12,3 +12,20 @@ public class nestLoop {
     }
   }
 }
+
+
+public class nestLoop {
+  public static void main(String[] args){
+
+    int [] [] twoDArray = {{2,4,8},
+                           {1,3,5}};
+
+    for (int i =0; i <twoDArray.length ; i++){
+        for (int j = 0; j <twoDArray[1].length; j++){ //twoDArray[1]是第二行，长度是3
+          System.out.println(twoDArray[i][j]); //这里是第一个要你做的事情，做到条件不允许了，再回上一个loop
+        }  //twoDArray[0][0]=2  twoDArray[0][1]=4 twoDArray[0][2]=8 twoDArray[0][3]= nonexistent
+      }
+    }
+  }
+  //总结如果你要print 2D Array里的所有东西， 列不能超过row的长度；而行不能超过2D Array的长度（这里的长度就是2，
+//也就是用逗号分割开来的2个行
